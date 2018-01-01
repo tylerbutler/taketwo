4d85b10f0a51f17c0775a7823b73ab0614fdee9a
Tyler Butler <tyler@tylerbutler.com>
2018-01-01 02:00:44 -0800

    Going nuts experimenting with gulp plugins

--- gulpfile.js
+++ gulpfile.js
@@ -5,20 +5,28 @@ var lint = require('remark-lint');
 var gap = require('remark-heading-gap');
 var shortcodes = require('remark-shortcodes');
 var squeezeParagraphs = require('remark-squeeze-paragraphs');
+var styleGuide = require('remark-preset-lint-markdown-style-guide');
+var gulpGrayMatter = require('gulp-gray-matter');
 
-gulp.task('default', () =>
-	gulp.src('content/**/*.md')
-	.pipe(remark().use(lint))
-	.pipe(gulp.dest('content'))
-);
+gulp.task('default', ['lint'], function () {
+	//gulp.src('content/**/*.md')
+	//.pipe(remark().use(lint))
+	//.pipe(gulp.dest('content'))
+});
 
-gulp.task('fix', () =>
+gulp.task('lint', () =>
 	gulp.src('content/**/*.md')
 	.pipe(remark()
 		//.use(shortcodes, {startBlock: "{{<", endBlock: ">}}"})
 		//.use(gap)
 		.use(lint)
-		.use(squeezeParagraphs)
+		//.use(squeezeParagraphs)
+		.use(styleGuide)
 	)
-	.pipe(gulp.dest('content'))
 );
+
+gulp.task('fix', function () {
+	return gulp.src('content/**/*.md')
+		.pipe(gulpGrayMatter({'remove': false}))
+		.pipe(gulp.dest('content'));
+});
